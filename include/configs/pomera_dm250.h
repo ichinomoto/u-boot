06caf4ec8a393eaf72b4ee09aca74b470156df57
ichinomoto <ichinomoto@gmail.com>
2026-09-12 11:58:13 +0900

    configs: pomera_dm250: Add U-Boot configuration
    Add the DM250 defconfig and board environment. Enable display, TC3589x keyboard input, MMC direct boot, USB gadget support and the DM250 boot command.

---
+++ include/configs/pomera_dm250.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+#ifndef __POMERA_DM250_H
+#define __POMERA_DM250_H
+
+#define BOOT_TARGETS "mmc1"
+#include <configs/rk3128_common.h>
+
+#undef CFG_EXTRA_ENV_SETTINGS
+#define CFG_EXTRA_ENV_SETTINGS \
+	ENV_MEM_LAYOUT_SETTINGS \
+	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"partitions=" PARTS_DEFAULT \
+	"boot_targets=" BOOT_TARGETS "\0" \
+	"stdin=serial,tc3589x-keyb\0" \
+	"stdout=serial,vidconsole\0" \
+	"stderr=serial,vidconsole\0"
+
+#endif
